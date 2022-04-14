42218a3c5c8d262f6017fcff4af3a11adb1106ad
lolomusa <101606435+lolomusa@users.noreply.github.com>
2022-04-14 16:27:13 -0300

    Add files via upload

---
+++ notas.sh
@@ -0,0 +1,8 @@
+memal@LAPTOP-OR1EBNMU MINGW64 ~/OneDrive/Desktop/UCEMA/F_informatica/github practice
+$ git add *
+
+memal@LAPTOP-OR1EBNMU MINGW64 ~/OneDrive/Desktop/UCEMA/F_informatica/github practice
+$ git commit -m "agregue archivo"
+
+memal@LAPTOP-OR1EBNMU MINGW64 ~/OneDrive/Desktop/UCEMA/F_informatica/github practice
+$ git push
