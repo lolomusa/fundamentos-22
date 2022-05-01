0d6d661581ed37cbce7e7d1056f3e070db85ee3d
lolomusa <lolymusa@icloud.com>
2022-05-01 16:41:31 -0300

    ok

--- examen/zejercicios .py
+++ examen/zejercicios .py
@@ -3,16 +3,14 @@ import re
 #2
 #3
 #4
-        import os
-        import glob
-        import re
+import os
+import glob
 
-        os.chdir(r"\Users\memal\OneDrive\Desktop\UCEMA\F_informatica\fundamentos-22\examen")
-        os.mkdir("Resultado")
+os.chdir(r"\Users\memal\OneDrive\Desktop\UCEMA\F_informatica\fundamentos-22\examen")
+os.mkdir("Resultado")
 
-        lista_archivos = glob.glob("*.txt")
-
-        with open("Resultado\texto_completo.txt", "a") as texto_completo:
-            for archivo in lista_archivos:
-                with open(archivo, "r") as file:
+lista_archivos = glob.glob("*.txt")
+with open("Resultado\texto_completo.txt", "a") as texto_completo:
+    for archivo in lista_archivos:
+        with open(archivo, "r") as file:
             texto_completo.write(file.read())
