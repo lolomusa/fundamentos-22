302741598555a5e4543daed0810ad6ca63d45a78
lolomusa <lolymusa@icloud.com>
2022-05-01 17:57:39 -0300

    new

--- examen2/ejercicios.py
+++ examen2/ejercicios.py
@@ -1,7 +1,39 @@
 #1
+#a)
+import re
+texto = "xcabcb3sabc9"
+patron = "abc"
+print(re.search(patron, texto))
+print(re.search(abc,xcabcb3sabc9))
+
+<re.Match object; span=(3, 6), match='abc'>
+
+import re
+texto = "xcabcb3sabc9"
+patron = "abc"
+print(re.findall(patron, texto))
+print(re.findall(abc,xcabcb3sabc9))
+
+['abc', 'abc']
+
+import re
+texto = "hola amigos mios"
+patron = "abc"
+print(re.search(patron, texto))
+print(re.search(abc,hola amigos mios))
+
+None
+
+import re
+texto = "hola amigos mios"
+patron = "abc"
+print(re.findall(patron, texto))
+print(re.findall(abc,hola amigos mios))
+
+[]
+
 #b)
 import re
-
 string = "aa([^c].*?)gg"  #aa literal | grupo de todo lo que no tenga c + cualquier caracter que se repita 0 o mas veces| gg literal
 patron = "ttaatatggttaacatgg"
 
@@ -9,3 +41,6 @@ def obtener_substrings(patron):
     print(re.findall(string, patron))
 
 obtener_substrings(patron)
+
+['tat']
+
