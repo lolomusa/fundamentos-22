14f7bed4fe289fdb18a823aa111a2d6b074fd441
lolomusa <lolymusa@icloud.com>
2022-05-03 10:56:11 -0300

    more

--- notas.sh
+++ notas.sh
@@ -1,4 +1,13 @@
 memal@LAPTOP-OR1EBNMU MINGW64 ~/OneDrive/Desktop/UCEMA/F_informatica/github practice
+$ ls
+Prueba-/  fundamentos-22/  practica_clonar/
+
+memal@LAPTOP-OR1EBNMU MINGW64 ~/OneDrive/Desktop/UCEMA/F_informatica/github practice
+$ cd practica_clonar/
+
+# Esto me posiciona en la carpeta q quiero para guardar
+
+memal@LAPTOP-OR1EBNMU MINGW64 ~/OneDrive/Desktop/UCEMA/F_informatica/github practice
 $ git add *
 
 memal@LAPTOP-OR1EBNMU MINGW64 ~/OneDrive/Desktop/UCEMA/F_informatica/github practice
